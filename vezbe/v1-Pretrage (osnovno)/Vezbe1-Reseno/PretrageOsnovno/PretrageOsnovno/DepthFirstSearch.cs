792edbbc03a17551a7a000d131fac678f4837cf9
Miroslav Kondic <Miroslav Kondic>
2017-03-25 12:33:43 +0100

    v2 - pretrage

---
+++ vezbe/v1-Pretrage (osnovno)/Vezbe1-Reseno/PretrageOsnovno/PretrageOsnovno/DepthFirstSearch.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace PretrageOsnovno
+{
+    class DepthFirstSearch
+    {
+        public State search(State pocetno)
+        {
+            List<State> stanjaZaObradu = new List<State>();
+
+            //TODO 6: Implementirati Prvi u dubinu
+            stanjaZaObradu.Add(pocetno);
+
+            while (stanjaZaObradu.Count > 0)
+            {
+                State naObradi = stanjaZaObradu[0];
+                if (naObradi.isFinalState())
+                {
+                    return naObradi;
+                }
+                stanjaZaObradu.RemoveAt(0);
+                List<State> mogucaSledecaStanja = naObradi.children();
+
+                stanjaZaObradu.InsertRange(0, mogucaSledecaStanja); // dfs
+            }
+
+            return null;
+        }
+    }
+}
---
+++ vezbe/v2- Pretrage/PretrageOsnovno/DepthFirstSearch.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace PretrageOsnovno
+{
+    class DepthFirstSearch
+    {
+        public State Search(string startNodeName, string endNodeName)
+        {
+            Node startNode = Program.instance.graph[startNodeName];
+            Node endNode = Program.instance.graph[endNodeName];
+
+            List<State> stanjaZaObradu = new List<State>();
+
+            stanjaZaObradu.Add(new State(startNode));
+
+            while (stanjaZaObradu.Count > 0)
+            {
+                State naObradi = stanjaZaObradu[0];
+                stanjaZaObradu.Remove(naObradi);
+
+                if (naObradi.Node.Name == endNode.Name)
+                {
+                    return naObradi;
+                }
+                else
+                {
+                    List<State> mogucaSledecaStanja = naObradi.children();
+                    stanjaZaObradu.InsertRange(0, mogucaSledecaStanja);
+                }
+            }
+            return null;
+        }
+    }
+}
