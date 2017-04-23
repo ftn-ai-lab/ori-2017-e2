0534f5e5b2a32e693140e86bbc9edd18b9df2e9b
Miroslav Kondic <Miroslav Kondic>
2017-04-23 14:36:47 +0200

    vezba 5

---
+++ vezbe/v5-Masinsko ucenje/MasinskoUcenje/MasinskoUcenje/Point.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MasinskoUcenje
+{
+    public class Point
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
