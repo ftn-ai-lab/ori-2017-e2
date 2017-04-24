4a3b1501b3fd3dba762283bdbf11bca26aad76ed
Miroslav Kondic <Miroslav Kondic>
2017-04-24 08:17:22 +0200

    Point int-> double

--- vezbe/v5-Masinsko ucenje/MasinskoUcenje/MasinskoUcenje/Point.cs
+++ vezbe/v5-Masinsko ucenje/MasinskoUcenje/MasinskoUcenje/Point.cs
@@ -7,10 +7,10 @@ namespace MasinskoUcenje
 {
     public class Point
     {
-        public int x { get; set; }
-        public int y { get; set; }
+        public double x { get; set; }
+        public double y { get; set; }
 
-        public Point(int x, int y)
+        public Point(double x, double y)
         {
             this.x = x;
             this.y = y;
